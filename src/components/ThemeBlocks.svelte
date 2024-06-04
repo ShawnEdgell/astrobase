8f6ea44df58191017f8104c2ca0a4ea06049be66
Shawn Edgell <milky.irl@gmail.com>
2024-06-04 17:50:42 -0400

    added theme pallet

--- src/components/ThemeBlocks.svelte
+++ src/components/ThemeBlocks.svelte
@@ -1,4 +1,4 @@
-<div class="flex space-x-4 mt-12">
+<div class="flex justify-center space-x-4 p-16">
     <div class="w-12 h-12 bg-primary"></div>
     <div class="w-12 h-12 bg-secondary"></div>
     <div class="w-12 h-12 bg-accent"></div>
